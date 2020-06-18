bb4fd9c9190642a7f304db5b8e8598025a57027a
yannikmaassen <yannik.maassen@t-online.de>
2020-06-18 15:53:23 +0200

    Add solution for FizzBuzz

---
+++ app/FizzBuzz.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+class FizzBuzz
+{
+
+  public function runFizzBuzz()
+  {
+    for ($i = 1; $i <= 100; $i++) {
+      if ($i % 3 === 0 && $i % 5 === 0) {
+        echo "FizzBuzz\n";
+      } elseif ($i % 3 === 0) {
+        echo "Fizz\n";
+      } elseif ($i % 5 === 0) {
+        echo "Buzz\n";
+      } else {
+        echo "$i\n";
+      }
+    }
+  }
+}
